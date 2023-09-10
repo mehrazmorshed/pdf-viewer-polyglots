1b742cd3878ef4c0a60eb6b1607f67f451e12e9c
Mehraz Morshed <mehrazmorshed@gmail.com>
2023-09-10 11:37:47 +0600

    Update pdf-viewer-polyglots.php

--- trunk/pdf-viewer-polyglots.php
+++ trunk/pdf-viewer-polyglots.php
@@ -7,7 +7,7 @@
  * Version:           1.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Tested up to:      6.3
+ * Tested up to:      6.3.1
  * Author:            Mehraz Morshed
  * Author URI:        https://profiles.wordpress.org/mehrazmorshed/
  * License:           GPL v2 or later
